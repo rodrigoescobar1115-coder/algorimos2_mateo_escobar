c4da6655ff49b4bfd3ce90b7c072208adadb12a4
Lab3 <rodrigoescobar1115@gmail.com>
2026-08-15 15:43:21 -0600

    commit inicial

---
+++ semana02/semana02/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace semana02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("esta en la semana 2");
+            
+        }
+    }
+}
