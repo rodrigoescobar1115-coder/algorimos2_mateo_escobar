62ec1e0c96765f1dabed10c46b06504a33422d68
Lab3 <Lab3@LAB3-093>
2026-08-15 15:13:29 -0600

    Agregar archivos de proyecto.

---
+++ laboratorio2/laboratorio2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace laboratorio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cliemte = "edgar";
+            int paso1 = 100;
+            int paso2 = 200;
+            Console.Write($"cliemte { cliemte} ha pagado" );
+            Console.WriteLine(paso1 + paso2);
+              
+
+        }
+    }
+}
---
+++ ladoratorio_2/ladoratorio_2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ladoratorio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre = "hola ana";
+            Console.WriteLine(nombre);
+        }
+    }
+}
